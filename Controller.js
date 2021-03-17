fec0e554d323710c07ad73c98d1594437f078e61
AnaAssuncao <apoassuncao@gmail.com>
2021-03-17 01:42:12 -0300

    Separação em class o controller

--- Controller.js
+++ Controller.js
@@ -3,13 +3,12 @@ import ViewController from "./View/ViewController.js"
 import interfaceNetwork from "./Network/Network.js"
 
 const playerConfig={
-    typeGame:null,
+    functionsGame:null,
     top:"Black",
     bottom:"White",
     currentPlayer:null,
     colorMultiPlayer:null
 }
-
 const game = new createGame(playerConfig)
 const startboard = game.getCurrentBoard()
 
@@ -19,190 +18,223 @@ const network = new interfaceNetwork()
 viewController.addHomePage()
 viewController.subscribeStartSinglePlayer(startSinglePlayer)
 viewController.subscribeStartMultiPlayer(startMultiPlayer)
-viewController.subscribeMovePiece(moveTypeGame)
-viewController.subscribeHistory(backPreviousMove)
-viewController.subscribeRestartGame(restartGame)
-
-network.subscribeMoveAdversary(getMoveAdv)
-network.subscribePlayerConnection(connectionPlayerTwo)
 
 function startSinglePlayer(){
-    viewController.clearHomePage()
-    playerConfig.currentPlayer="White"
-    playerConfig.typeGame="SinglePlayer"
-    const msgConnection="Jogo Local"
-    viewController.updateStatusConection("offline",msgConnection)
-    viewController.addBackMovement()
-    startGame(playerConfig.currentPlayer, playerConfig.top)
+    playerConfig.functionsGame = new offline()
+    viewController.subscribeMovePiece(playerConfig.functionsGame.move)
+    viewController.subscribeHistory(playerConfig.functionsGame.backPreviousMove)
+    viewController.subscribeRestartGame(playerConfig.functionsGame.restartGame)
+    playerConfig.functionsGame.start()
 }
 
-async function startMultiPlayer(infGame){
-    // infGame = {name:value, roomCode:value}
-    const infCode= await network.sendSever.infStartGame(infGame)
-    if(infCode.connectedServer){
+function startMultiPlayer(infGame){
+    playerConfig.functionsGame = new online()
+    viewController.subscribeMovePiece(playerConfig.functionsGame.move)
+    viewController.subscribeRestartGame(playerConfig.functionsGame.restartGame)
+    network.subscribeMoveAdversary(playerConfig.functionsGame.getMoveAdv)
+    network.subscribePlayerConnection(playerConfig.functionsGame.connectionPlayerTwo)
+    playerConfig.functionsGame.start(infGame)
+}
+
+class offline{
+    start(){
         viewController.clearHomePage()
-        playerConfig.typeGame="MultiPlayer"
-        if(infCode.playerAdv.connection===false){
-            const msgConnection = "Aguardando adversário"
-            viewController.updateStatusConection("online",msgConnection)
-            playerConfig.colorMultiPlayer="White"
-            playerConfig.currentPlayer =playerConfig.colorMultiPlayer
-            const isPlayable = false
-            startGame(playerConfig.currentPlayer, playerConfig.top, isPlayable)
-            network.enableCalls.playerConnection()
+        playerConfig.currentPlayer="White"
+        const connection={
+            msgConnection:"Jogo Local",
+            type:"offline"
         }
-        else{
-            const msgConnection = "Conectado com "+ infCode.playerAdv.namePlayer
-            viewController.updateStatusConection("online",msgConnection)
-            playerConfig.colorMultiPlayer="Black"
-            playerConfig.currentPlayer="White"
-            const isPlayable = false
-            startGame(playerConfig.currentPlayer, playerConfig.top, isPlayable)
-            network.enableCalls.moveAdversary()
-        }
-        network.enableCalls.statusGame()
+        viewController.updateStatusConection(connection.type,connection.msgConnection)
+        viewController.addBackMovement()
+        generalFunctions.startGame(playerConfig.currentPlayer, playerConfig.top)
     }
-    else{
-        viewController.informationProminent(infCode.msg)
-    }
-}
 
-function connectionPlayerTwo(infPlayerAdv){
-    if(infPlayerAdv.connection===false){
-        viewController.addEndGameInformation("noAdv")
-        setTimeout(()=>{
-            viewController.clearinformationModal()
-            viewController.addHomePage()
-        },5000)
-    }
-    else{
-        const msgConnection = "Conectado com "+ infPlayerAdv.namePlayer
-        viewController.updateStatusConection("online",msgConnection)
-        startGame(playerConfig.currentPlayer, playerConfig.top)
-    }
-}
-    
-
-function startGame(colorPlayer, colorTop, isPlayable){
-    game.starObjGame(colorPlayer)
-    const chessBoard=game.getCurrentBoard()
-    const statusGame = game.getStatusGame() 
-    const capturedPieces = game.getCapturedPieces()
-    const playHistory = game.getHistoryMoves()
-    updateBoard(colorPlayer,chessBoard,isPlayable) 
-    updateInformationGame(colorPlayer,statusGame)
-    updateCapturedPiece(colorTop,capturedPieces)
-    updatePlaysHistory(playHistory)   
-}
-
-function moveTypeGame(informationPieceSelect){
-    if(playerConfig.typeGame==="SinglePlayer"){
+    move(informationPieceSelect){
         const nextPlayer=(playerConfig.top===playerConfig.currentPlayer)?playerConfig.bottom:playerConfig.top
-        const isMove = movePiece(informationPieceSelect,nextPlayer)
+        const isMove =generalFunctions.movePiece(informationPieceSelect,nextPlayer)
         if(isMove){
             playerConfig.currentPlayer=nextPlayer
         }
     }
-    else if(playerConfig.typeGame==="MultiPlayer"){
+
+    backPreviousMove(){
+        const playHistory = game.getHistoryMoves()
+        if(playHistory.length>0){
+            game.returnMovement()
+            const pastColor=(playerConfig.top===playerConfig.currentPlayer)?playerConfig.bottom:playerConfig.top
+            const chessBoard=game.getCurrentBoard()
+            const statusGame = game.getStatusGame() 
+            const capturedPieces = game.getCapturedPieces()
+            const playHistory = game.getHistoryMoves()
+            if(statusGame.endGame===false){
+                generalFunctions.updateBoard(pastColor,chessBoard)        
+                generalFunctions.updateInformationGame(pastColor,statusGame)
+                generalFunctions.updateCapturedPiece(playerConfig.top,capturedPieces)        
+                generalFunctions.updatePlaysHistory(playHistory)   
+                playerConfig.currentPlayer=pastColor
+            }
+        }
+    }
+
+    restartGame(){
+        playerConfig.functionsGame = null
+        playerConfig.currentPlayer= null
+        playerConfig.colorMultiPlayer= null
+        viewController.clearEndGameInformation()
+        viewController.clearBackMovement()
+        viewController.addHomePage()
+    }
+}
+
+class online{
+    async start(infGame){
+        // infGame = {name:value, roomCode:value}
+        const infCode= await network.sendSever.infStartGame(infGame)
+        if(infCode.connectedServer){
+            viewController.clearHomePage()
+            if(infCode.playerAdv.connection===false){
+                const connection={
+                    msgConnection: "Aguardando adversário",
+                    type:"online"
+                } 
+                viewController.updateStatusConection(connection.type,connection.msgConnection)
+                playerConfig.colorMultiPlayer="White"
+                playerConfig.currentPlayer =playerConfig.colorMultiPlayer
+                const isPlayable = false
+                generalFunctions.startGame(playerConfig.currentPlayer, playerConfig.top, isPlayable)
+                network.enableCalls.playerConnection()
+            }
+            else{
+                const connection={
+                    msgConnection: ("Conectado com "+ infCode.playerAdv.namePlayer),
+                    type:"online"
+                } 
+                viewController.updateStatusConection(connection.type,connection.msgConnection)
+                playerConfig.colorMultiPlayer="Black"
+                playerConfig.currentPlayer="White"
+                const isPlayable = false
+                generalFunctions.startGame(playerConfig.currentPlayer, playerConfig.top, isPlayable)
+                network.enableCalls.moveAdversary()
+            }
+            network.enableCalls.statusGame()
+        }
+        else{
+            viewController.informationProminent(infCode.msg)
+        }
+    }
+    
+    connectionPlayerTwo(infPlayerAdv){
+        if(infPlayerAdv.connection===false){
+            viewController.addEndGameInformation("noAdv")
+            setTimeout(()=>{
+                viewController.clearinformationModal()
+                viewController.addHomePage()
+            },5000)
+        }
+        else{
+            const connection={
+                msgConnection: ("Conectado com "+ infPlayerAdv.namePlayer),
+                type:"online"
+            } 
+            viewController.updateStatusConection(connection.type,connection.msgConnection)
+            generalFunctions.startGame(playerConfig.currentPlayer, playerConfig.top)
+        }
+    }
+
+    move(informationPieceSelect){
         const isPlayable=false
         const nextPlayer=(playerConfig.top===playerConfig.currentPlayer)?playerConfig.bottom:playerConfig.top
-        const isMove = movePiece(informationPieceSelect,nextPlayer,isPlayable)
+        const isMove = generalFunctions.movePiece(informationPieceSelect,nextPlayer,isPlayable)
         if(isMove){
             // {informationPieceSelect: fullName,color,typeMovement,specialMovement,refId e piecePromotion}
             const infSendMove = network.sendSever.moveGame(informationPieceSelect)
-            if(infSendMove.connectedServer===true){
-                network.enableCalls.moveAdversary()
-            }
-            else{
-                viewController.informationProminent(infCode.msg)
-            }
+            network.enableCalls.moveAdversary()
         }
     }
-}
 
-function getMoveAdv(informationPieceSelect){
-    if(informationPieceSelect===false){
-        viewController.informationProminent(infCode.msg)
-    }
-    else{
-        const nextPlayer=playerConfig.colorMultiPlayer
-        const isMove = movePiece(informationPieceSelect,nextPlayer)
-        if(isMove){
-            // network.sendSever.recMoveGame("true")
+    getMoveAdv(informationPieceSelect){
+        if(informationPieceSelect===false){
+            viewController.informationProminent(infCode.msg)
         }
         else{
-            // enviar caso tiver movimento invalido
-            // network.sendSever.recMoveGame("false")
+            const nextPlayer=playerConfig.colorMultiPlayer
+            const isMove = generalFunctions.movePiece(informationPieceSelect,nextPlayer)
+            if(isMove){
+                // network.sendSever.recMoveGame("true")
+            }
+            else{
+                // enviar caso tiver movimento invalido
+                // network.sendSever.recMoveGame("false")
+            }
         }
     }
-}
 
-function movePiece(informationPieceSelect,colorPlayer,isPlayable){
-    let isMove = false
-    if(informationPieceSelect.specialMovement){
-        isMove=game.informSpecialMovement(informationPieceSelect)
+    restartGame(){
+        playerConfig.functionsGame = null
+        playerConfig.currentPlayer= null
+        playerConfig.colorMultiPlayer= null
+        // giveUp
+        viewController.clearEndGameInformation()
+        viewController.clearBackMovement()
+        viewController.addHomePage()
     }
-    else{
-        isMove=game.informMove(informationPieceSelect) 
-    }  
-    const chessBoard=game.getCurrentBoard()
-    const statusGame = game.getStatusGame() 
-    const capturedPieces = game.getCapturedPieces()
-    const playHistory = game.getHistoryMoves()
-    if(statusGame.endGame){
-        isPlayable=false
-    }
-    updateBoard(colorPlayer,chessBoard,isPlayable) 
-    updateInformationGame(colorPlayer,statusGame)
-    updateCapturedPiece(playerConfig.top,capturedPieces)
-    updatePlaysHistory(playHistory)   
-    return isMove
 }
 
-function updateBoard(colorPlayer,board,isPlayable=true){
-    viewController.updateBoard(board,colorPlayer,isPlayable) 
-}
-
-function updateInformationGame(colorPlayer,statusGame){  
-    viewController.updateStatusGame(statusGame,colorPlayer)
-    if(statusGame.endGame===true && statusGame.checkMate===false){
-        const status ={
-            endGame:statusGame.endGame,
-            playerConfigWin:statusGame.playerConfigWin
-        }
-        viewController.endGame(status,colorPlayer)
-    } 
-}
-
-function updateCapturedPiece(colorTop,capturedPieces){
-    viewController.updateCapturedPieces(colorTop,capturedPieces)
-}
-
-function updatePlaysHistory(history){
-    viewController.updateHistory(history)
-}
-
-function backPreviousMove(){
-    const playHistory = game.getHistoryMoves()
-    if(playHistory.length>0){
-        game.returnMovement()
-        const pastColor=(playerConfig.top===playerConfig.currentPlayer)?playerConfig.bottom:playerConfig.top
+class generalFunctions{
+     static startGame(colorPlayer, colorTop, isPlayable){
+        game.starObjGame(colorPlayer)
         const chessBoard=game.getCurrentBoard()
         const statusGame = game.getStatusGame() 
         const capturedPieces = game.getCapturedPieces()
         const playHistory = game.getHistoryMoves()
-        if(statusGame.endGame===false){
-            updateBoard(pastColor,chessBoard)        
-            updateInformationGame(pastColor,statusGame)
-            updateCapturedPiece(playerConfig.top,capturedPieces)        
-            updatePlaysHistory(playHistory)   
-            playerConfig.currentPlayer=pastColor
-        }
+        this.updateBoard(colorPlayer,chessBoard,isPlayable) 
+        this.updateInformationGame(colorPlayer,statusGame)
+        this.updateCapturedPiece(colorTop,capturedPieces)
+        this.updatePlaysHistory(playHistory)   
     }
-}
-
-function restartGame(){
-    viewController.addHomePage()
-    viewController.clearEndGameInformation()
-    viewController.clearBackMovement()
+    
+    static movePiece(informationPieceSelect,colorPlayer,isPlayable){
+        let isMove = false
+        if(informationPieceSelect.specialMovement){
+            isMove=game.informSpecialMovement(informationPieceSelect)
+        }
+        else{
+            isMove=game.informMove(informationPieceSelect) 
+        }  
+        const chessBoard=game.getCurrentBoard()
+        const statusGame = game.getStatusGame() 
+        const capturedPieces = game.getCapturedPieces()
+        const playHistory = game.getHistoryMoves()
+        if(statusGame.endGame){
+            isPlayable=false
+        }
+        this.updateBoard(colorPlayer,chessBoard,isPlayable) 
+        this.updateInformationGame(colorPlayer,statusGame)
+        this.updateCapturedPiece(playerConfig.top,capturedPieces)
+        this.updatePlaysHistory(playHistory)   
+        return isMove
+    }
+    
+    static updateBoard(colorPlayer,board,isPlayable=true){
+        viewController.updateBoard(board,colorPlayer,isPlayable) 
+    }
+    
+    static updateInformationGame(colorPlayer,statusGame){  
+        viewController.updateStatusGame(statusGame,colorPlayer)
+        if(statusGame.endGame===true && statusGame.checkMate===false){
+            const status ={
+                endGame:statusGame.endGame,
+                playerConfigWin:statusGame.playerConfigWin
+            }
+            viewController.endGame(status,colorPlayer)
+        } 
+    }
+    
+    static updateCapturedPiece(colorTop,capturedPieces){
+        viewController.updateCapturedPieces(colorTop,capturedPieces)
+    }
+    
+    static updatePlaysHistory(history){
+        viewController.updateHistory(history)
+    }
 }
