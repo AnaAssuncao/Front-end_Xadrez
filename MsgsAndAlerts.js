cbfa40844b5044665fde2d1392e66be092c8a6c9
AnaAssuncao <apoassuncao@gmail.com>
2021-10-31 16:26:34 -0300

    Alteração de português para inglês

--- MsgsAndAlerts.js
+++ MsgsAndAlerts.js
@@ -1,71 +1,71 @@
 
 const colors={
-    white:"Brancas",
-    black:"Pretas"
+    white:"White",
+    black:"Black"
 }
 
 const roomAndCode={
-    roomWithOnePlayer:()=> "Esta sala já foi criada, conecta ao adversário",
-    roomUnavailable:()=>"Nesta sala já está acontecendo um jogo, tente outro código",
-    connectedRoom:(code)=>"Conectado a sala " + code,
-    noExistRoom:()=>"Está sala não existe, cria esta sala.",
-    waitAgain:()=>"Não é sua vez de jogar, aguarde." ,
-    reconnectRoom:()=>"Sala reconectada",
-    roomNotReconnected:()=>"Sala não reconectada",
-    runningGame:()=>"Possivelmente existe uma aba de jogo aberta, feche e recarregue esta. Caso não estiver, recarregue a página após 5 segundos."
+    roomWithOnePlayer:()=> "This room has already been created, connect to the adversary",
+    roomUnavailable:()=>"Ocupied room code, try another code",
+    connectedRoom:(code)=>"Connected to room " + code,
+    noExistRoom:()=>"This room does not exist, create this room.",
+    waitAgain:()=>"It's not your turn to play, wait." ,
+    reconnectRoom:()=>"Reconnected room",
+    roomNotReconnected:()=>"Room not reconnected",
+    runningGame:()=>"Possibly there is an open game tab, close and reload this. If not, reload the page after 5 seconds."
 }
 
 const connection={
-    noConnection:()=>"Sem conexão com o servidor, aguarde ou jogue local.",
-    errServer:()=>"Ocorreu um erro no servidor, por favor recarregue a página.",
-    place:()=>"Jogo Local",
-    waitAdv:()=>"Aguardando adversário",
-    connected:(playerName)=>"Adversário(a) "+ playerName
+    noConnection:()=>"No server connection, wait or play local.",
+    errServer:()=>"There was an error on the server, please reload the page.",
+    place:()=>"Offline Game",
+    waitAdv:()=>"Awaiting adversary",
+    connected:(playerName)=>"Adversary "+ playerName
 }
 
 const checksPiece={
-    checkMate:(color)=> "Xeque-Mate nas Peças " + colors[color],
-    check:(color)=>"Xeque nas Peças " + colors[color],
-    noCheck:()=>""
+    checkMate:(color)=> "Checkmate in the " + colors[color] + " Pieces",
+    check:(color)=>"Check in the " + colors[color] + " Pieces",
+    noCheck:()=>"No Check"
 }
 
 const checksPlayers={
-    checkMate:(playerName)=>"Xeque-Mate no(a) Jogador(a) " + playerName,
-    check:(playerName)=>"Xeque no(a) Jogador(a) " + playerName,
-    noCheck:()=>""
+    checkMate:(playerName)=>"Checkmate on the Player " + playerName,
+    check:(playerName)=>"Check on the Player " + playerName,
+    noCheck:()=>"No Check"
 }
     
 const endGame={
-    winPiece:(color)=>"Vitória das Peças " + colors[color],
-    winPlayer:(playerName)=>"Vitória do(a) Jogador(a) " + playerName,
-    giveUpPlayer:(playerName)=>"Desistência do(a) Jogador(a) " + playerName,
-    draw:()=>"Jogo empatado",
-    timeOutToMovePiece:(color)=>"Tempo esgotado para mover Peças " + colors[color],
-    timeOutToMovePlayer:(playerName)=>"Tempo esgotado para mover Jogador(a) " + playerName
+    winPiece:(color)=>"Victory of the " + colors[color] + " Pieces",
+    winPlayer:(playerName)=>"Player victory " + playerName,
+    giveUpPlayer:(playerName)=>"Player " + playerName +" has surrendered",
+    draw:()=>"Tied game",
+    timeOutToMovePiece:(color)=>"Time out to move " + colors[color] + " Pieces",
+    timeOutToMovePlayer:(playerName)=>"Time out to move Player" + playerName
 }
 
 const startGame={
-    startGame:()=>"Jogo iniciado",
-    colorPlayer:(color)=>"A cor da sua Peça é " + colors[color],
-    noAdv:()=> "Sem adversario"
+    startGame:()=>"Game started",
+    colorPlayer:(color)=>"The color of your Pieces are " + colors[color],
+    noAdv:()=> "No adversary"
 }
 
 const movement={
-    movementPiece:(color)=>"Movimento das Peças " + colors[color],
-    movementPlayer:(color,name)=>"Movimento das Peças " + colors[color] + " Jodador(a) " + name,
-    return:(color)=>"Retorno das Peças " + colors[color],
-    nextColor:(color)=>"Vez das Peças " + colors[color],
-    nextPlayer:(name)=>"Vez do Jogador(a) " + name,
-    yourTurn:(name)=>name + " sua vez de jogar",
-    nextColorAndPlayer:(color,name)=>"Vez das Peças " + colors[color]+" Vez do Jogador(a) " + name,
-    incorrectMovement:(color)=>"Erro na jogada das Peças " + colors[color],
-    cheatMovement:()=>"Movimento de trapaça",
-    endTime:()=>"Tempo esgotado para adversário",
-    moveAgain:()=>"Movimenta novamente",
-    correctMovement:()=>"Movimento correto"
+    movementPiece:(color)=>"Movement of " + colors[color] + " Pieces",
+    movementPlayer:(color,name)=>"Movement of " + colors[color] + " Pieces - Player    " + name,
+    return:(color)=>"Return of " + colors[color] + " Pieces",
+    nextColor:(color)=>"Pieces turn: " + colors[color],
+    nextPlayer:(name)=>"Player turn: " + name,
+    yourTurn:(name)=>name + " your turn to play",
+    nextColorAndPlayer:(color,name)=>"Pieces turn: " + colors[color]+" - Player turn: " + name,
+    incorrectMovement:(color)=>"Error in playing the Pieces " + colors[color],
+    cheatMovement:()=>"Cheat move",
+    endTime:()=>"Time has ended to player",
+    moveAgain:()=>"Move again",
+    correctMovement:()=>"Correct movement"
 }
 const log={
-    connected:(playerName)=>"Conectado com " + playerName,
+    connected:(playerName)=>"Connected with " + playerName,
     gamelog:(msg)=>{
         const time = new Date()
         const hours = formatTime(time.getHours())
