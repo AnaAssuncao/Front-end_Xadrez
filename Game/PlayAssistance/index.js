9be8f9ac337ac9d51269a4581b44e9d3a4b8e8e3
AnaAssuncao <apoassuncao@gmail.com>
2021-11-05 19:09:29 -0300

    Alteração para importar arquivos da pasta

--- Game/PlayAssistance/index.js
+++ Game/PlayAssistance/index.js
@@ -1,17 +1,22 @@
-
+import assistantKing from "./AssistantKing.js"
+import checkAssistance from "./CheckAssistance.js"
+import assistantPiece from "./AssistantPiece.js"
 
 function updateFilterMovement(color, statusGame=this.statusGame){
     if(statusGame.checkKing.checkMate===false){
         if(statusGame.checkKing.check===true ){
-            this.checkAssistance(color)
+            checkAssistance.apply(this,[color])
         }
         else{
-            this.assistantKing(color)
-            this.assistantPiece(color)
+            assistantKing.apply(this,[color])
+            assistantPiece.apply(this,[color])
         }
     }
 }
 
 export default {
+    assistantKing,
+    checkAssistance,
+    assistantPiece,
     updateFilterMovement
 }
