c2daa0186780a8e26fb22466f415fee8e362da92
AnaAssuncao <apoassuncao@gmail.com>
2021-11-05 01:59:50 -0300

    Separação da função VerifyCheck

---
+++ Game/UpdateStatusGame/VerifyCheck.js
@@ -0,0 +1,19 @@
+import {movementsPieceAdversity} from "./utils.js"
+
+function verifyCheck(refIdKing,colorKing,piecesBoard=this.piecesBoard){
+    let checks = {
+        qt:0,
+        pieceCheck:null
+    }
+    for(let piece in piecesBoard.pieces){
+        if((colorKing!== piecesBoard.pieces[piece].color)&&( piecesBoard.pieces[piece].isAtive===true)){
+            if(movementsPieceAdversity (piecesBoard.pieces[piece].refMovements,refIdKing)){
+                checks.qt++
+                checks.pieceCheck=piece
+            }
+        }
+    }
+    return checks
+}
+
+export default verifyCheck
