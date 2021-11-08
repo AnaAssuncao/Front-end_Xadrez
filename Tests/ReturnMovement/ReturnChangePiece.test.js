6d1cebb1b795d07eb115a48f3c50c1f5cc24d9a3
AnaAssuncao <apoassuncao@gmail.com>
2021-11-08 17:52:31 -0300

    Realização de testes para o arquivo ReturnChangePiece

---
+++ Tests/ReturnMovement/ReturnChangePiece.test.js
@@ -0,0 +1,37 @@
+import returnChangePiece from "../../Game/ReturnMovement/ReturnChangePiece.js"
+import makePiece from "../../Game/DefaultsObjects/FactoryMakePiece.js"
+import defaultsObjects from "../../Game/DefaultsObjects/index.js"
+import movementsPossibilities from "../../Game/MovementsPossibilities/index.js"
+
+const chessBoard = new defaultsObjects.ClassChessBoard()
+const piecesBoard = new defaultsObjects.ClassPiecesBoard()
+const playHistory = new defaultsObjects.ClassPlayHistory()
+const capturedPiece = new defaultsObjects.ClassCapturedPiece ()
+
+const nameKing = "Kingwhite"
+const lastRefKing = 'ref11'
+const lastKing = makePiece.apply({chessBoard},["King",nameKing,"white","kingWhite",lastRefKing,movementsPossibilities.king])
+piecesBoard.addPieceOfRef(nameKing,lastKing)
+lastKing.changePosition(lastRefKing)
+chessBoard.addPieceOfRef(lastRefKing,lastKing)
+
+const lastHistory = {
+    newRefId: [lastRefKing],
+    pieceCaptured: null,
+    piecesPlayed: [{...lastKing}],
+    typeMovement: "movementPiece"
+}
+playHistory.setHistory(lastHistory)
+const newKing = {...lastKing}
+const newRefKing = "ref18"
+newKing.changePosition(newRefKing)
+chessBoard.deletePieceOfRef(lastRefKing ,newKing)
+chessBoard.addPieceOfRef(newRefKing,newKing)
+
+describe("Return Movement - Change Piece", ()=>{
+    test("Should return movement ",()=>{
+        returnChangePiece(chessBoard,piecesBoard,playHistory,capturedPiece)
+        expect(chessBoard.reference[lastRefKing]).toEqual(lastKing)
+    })
+    
+})
