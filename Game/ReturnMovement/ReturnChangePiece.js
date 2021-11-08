c40db6058f293d1e54f49b981d86eda80377ee7e
AnaAssuncao <apoassuncao@gmail.com>
2021-11-08 16:20:08 -0300

    Separação da função returnChangePiece

---
+++ Game/ReturnMovement/ReturnChangePiece.js
@@ -0,0 +1,31 @@
+function returnChangePiece(chessBoard=this.chessBoard,piecesBoard=this.piecesBoard,playHistory=this.playHistory,capturedPiece=this.capturedPiece){
+    const lastMovement= playHistory.history.length-1
+    if(lastMovement>=0){
+        playHistory.history[lastMovement].piecesPlayed.forEach((piecesPlayed,ind)=>{
+            if(playHistory.history[lastMovement].typeMovement==="piecePromotion" && ind===1){
+                const namePiece=piecesPlayed.fullName
+                piecesBoard.deletePiece(namePiece)
+            }
+            else{
+                const position = playHistory.history[lastMovement].newRefId[ind]
+                chessBoard.deletePieceOfRef(position)
+                if(playHistory.history[lastMovement].pieceCaptured!==null){
+                    const namePieceCaptured =playHistory.history[lastMovement].pieceCaptured.fullName
+                    const pastPiece = playHistory.history[lastMovement].pieceCaptured
+                    pastPiece.enablePiece()
+                    const positionPieceCaptured=playHistory.history[lastMovement].pieceCaptured.position
+                    chessBoard.addPieceOfRef(positionPieceCaptured,pastPiece)
+                    capturedPiece.deleteCapturedPiece(namePieceCaptured)
+                }
+                const positionBack = piecesPlayed.position
+                const fullNameBack = piecesPlayed.fullName
+                piecesPlayed.changePosition(positionBack)
+                piecesBoard.addPieceOfRef(fullNameBack,piecesPlayed)
+                chessBoard.addPieceOfRef(positionBack,piecesPlayed)
+            }
+        })
+        playHistory.deleteLastHistory ()
+    } 
+}
+
+export default returnChangePiece
