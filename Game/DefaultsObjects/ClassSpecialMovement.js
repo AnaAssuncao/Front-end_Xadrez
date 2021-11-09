95778ee5007b959fca410513e3fc32c05f8faae7
AnaAssuncao <apoassuncao@gmail.com>
2021-11-09 16:56:13 -0300

    Acréscimo de funções na classe ClassSpecialMovement

--- Game/DefaultsObjects/ClassSpecialMovement.js
+++ Game/DefaultsObjects/ClassSpecialMovement.js
@@ -11,7 +11,7 @@ export default class SpecialMovement{
         this.enPassant={
             isPossible:false,
             pawnPossibleCapture:null,
-            refIdPawn:null,
+            refIdAtack:null,
             pawnInAtack:[]
         }
         this.pawnPromotion={
@@ -19,5 +19,24 @@ export default class SpecialMovement{
             piecesPawn:[],   
             namesPawn:[]  
         }  
+    }
+    addRoqueMovement(isPossible,king,possibleMovement,tower,refMovementTower){
+        this.roque.isPossible=isPossible
+        this.roque.king= king
+        this.roque.positionKingToRoque.push(possibleMovement)
+        this.roque.tower.push(tower)
+        this.roque.newMovementTower.push(refMovementTower)
+        console.log(this.roque)
+    }
+    addEnPassantMovement(isPossible,pawnPossibleCapture,newMovementPiece,pawnInAtack){
+        this.enPassant.isPossible=isPossible
+        this.enPassant.pawnPossibleCapture=pawnPossibleCapture
+        this.enPassant.refIdAtack=newMovementPiece
+        this.enPassant.pawnInAtack=pawnInAtack    
+    }
+    addPawnPromotion(pawn,namePawn){
+        this.pawnPromotion.isPossible=true
+        this.pawnPromotion.piecesPawn.push( pawn)
+        this.pawnPromotion.namesPawn.push(namePawn)
     }
 }
