2a6095924147c6722bee9399f821aa883fd71ab5
AnaAssuncao <apoassuncao@gmail.com>
2021-11-10 00:09:48 -0300

    Separação da função CreateNewPiece

---
+++ Game/CreateNewPiece/CreateNewPiece.js
@@ -0,0 +1,24 @@
+import defaultObjets from "../DefaultsObjects/index.js"
+import movementsPossibilities from "../MovementsPossibilities/index.js"
+
+function createNewPiece(informationNewPiece,pawnPiece,piecesPromotion){
+    const chancePiece={
+        names:["Tower","Knight","Bishop","Queen"],
+        functionPieces:[movementsPossibilities.tower,movementsPossibilities.knight,movementsPossibilities.bishop,movementsPossibilities.queen],
+        fullName:{
+            black:["towerBlack","knightBlack","bishopBlack","queenBlack"],
+            white:["towerWhite","knightWhite","bishopWhite","queenWhite"]
+        }
+    }
+    const color = pawnPiece.color
+    const indChangePiece= chancePiece.fullName[color].indexOf(informationNewPiece.piecePromotion)
+    const namePiece=`${chancePiece.names[indChangePiece]}-${piecesPromotion.qtPiece[color][indChangePiece]}`
+    const fullName=namePiece+color
+    const img = informationNewPiece.piecePromotion
+    const position = informationNewPiece.refId
+    const functionPiece = chancePiece.functionPieces[indChangePiece]
+    const newPiece = defaultObjets.factoryMakePiece.apply(this,[namePiece,fullName,color,img,position,functionPiece])
+    piecesPromotion.addPiecesPromotion(color,newPiece,pawnPiece.qtMovements,indChangePiece)
+}
+
+export default createNewPiece
