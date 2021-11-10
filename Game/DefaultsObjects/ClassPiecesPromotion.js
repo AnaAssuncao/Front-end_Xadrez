61322921a651796b9bfeb221f3cfe3aa52eb7f0d
AnaAssuncao <apoassuncao@gmail.com>
2021-11-09 23:59:20 -0300

    Alterações para implementação dos movimentos especiais

--- Game/DefaultsObjects/ClassPiecesPromotion.js
+++ Game/DefaultsObjects/ClassPiecesPromotion.js
@@ -3,10 +3,15 @@ export default class PiecesPromotion{
         this.chancePiece=false,
         this.promotedPawn=null,
         this.color=null,
-        this.black={
-            qtPiece:[1,1,1,1]},
-        this.white={
-            qtPiece:[1,1,1,1]},
+        this.qtPiece={
+            black:[1,1,1,1],
+            white:[1,1,1,1],
+        }
         this.newPiece=null
+    }
+    addPiecesPromotion(color,newPiece,qtMovements,indChangePiece){
+        this.newPiece=newPiece
+        this.newPiece.changeMovementsPossibilities(qtMovements)
+        this.qtPiece[color][indChangePiece]++
     }
 }
--- Game/MakeMovements/index.js
+++ Game/MakeMovements/index.js
@@ -1,8 +1,13 @@
 import changePiecePosition from "./ChangePiecePosition.js"
 import eatPiece from "./EatPiece.js"
-
+import movementRoque from "./movementRoque.js"
+import movementEnPassant from "./MovementEnPassant.js"
+import movementPawnPromotion from "./MovementPawnPromotion.js"
 
 export default {
     changePiecePosition,
     eatPiece,
+    movementRoque,
+    movementEnPassant,
+    movementPawnPromotion
 }
